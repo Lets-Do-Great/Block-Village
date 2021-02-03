35e7fcfcfcdb66804689046635dc064dc44be6e7
parkbum <parkbum11@naver.com>
2021-02-03 12:03:47 +0900

    [fix] background img size

--- frontend/src/components/main/main/main.js
+++ frontend/src/components/main/main/main.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import styles from './main.module.css'
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
@@ -6,10 +6,54 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { Route, Switch, useHistory } from 'react-router-dom';
 import Navbar from '../navbar/navbar';
 import MainMenu from '../main_menu/main_menu';
+import Draggable from 'react-draggable';
 
 const Main = (props) => {
   const history = useHistory();
-  const scroll_main = useRef();
+  const main_image = useRef();
+
+  useEffect(() => {
+
+    const ele = main_image.current;
+
+    ele.style.cursor = 'grab';
+
+    let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+    const mouseDownHandler = function(e) {
+
+      ele.style.cursor = 'grabbing';
+      ele.style.userSelect = 'none';
+      pos = {
+        left: ele.scrollLeft,
+        top: ele.scrollTop,
+        x: e.clientX,
+        y: e.clientY,
+      };
+      window.addEventListener('mousemove', mouseMoveHandler);
+      window.addEventListener('mouseup', mouseUpHandler);
+    };
+
+    const mouseMoveHandler = function(e) {
+      const dx = e.clientX - pos.x;
+      const dy = e.clientY - pos.y;
+
+      ele.scrollTop = pos.top - dy;
+      ele.scrollLeft = pos.left - dx;
+    };
+
+    const mouseUpHandler = function() {
+      ele.style.cursor = 'grab';
+      ele.style.removeProperty('user-select');
+
+      window.removeEventListener('mousemove', mouseMoveHandler);
+      window.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    ele.addEventListener('mousedown', mouseDownHandler);
+
+  })
+
 
   const card_infos = [
     {
@@ -55,18 +99,22 @@ const Main = (props) => {
 
   return (
     <div className={styles.body}>
-      
       <Navbar />
+      <div ref={main_image}>
+        <img 
+          src="/images/main_background_image.png" 
+          className={styles.background_img} 
+          >
+        </img>
+      </div>
 
+      <div className={styles.container} >
 
-      <div 
-        className={styles.container} 
-        ref={scroll_main} 
-        id="main_container">
-
-        <div className={styles.goMission} onClick={goMission}></div>
+        {/* <div className={styles.goMission} onClick={goMission}></div>
         <div className={styles.goStore} onClick={goStore}></div>
-        <div className={styles.goChallenge} onClick={goChallenge}></div>
+        <div className={styles.goChallenge} onClick={goChallenge}></div> */}
+
+
         {/* <div className={styles.left_btn}>
           <FaChevronLeft size="50" />
         </div> */}
