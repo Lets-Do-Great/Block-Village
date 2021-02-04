a10d4c67ccfb677031e0426c4e1cbef3ee023620
KGB <kbsam2@gmail.com>
2021-02-05 01:34:06 +0900

    [fix] response

--- backend/src/main/java/com/ssafy/edu/service/challenge/ChallengeServiceImpl.java
+++ backend/src/main/java/com/ssafy/edu/service/challenge/ChallengeServiceImpl.java
@@ -1,10 +1,7 @@
 package com.ssafy.edu.service.challenge;
 
 
-import com.ssafy.edu.model.challenge.Challenge;
-import com.ssafy.edu.model.challenge.ChallengeResponse;
-import com.ssafy.edu.model.challenge.ChallengeUser;
-import com.ssafy.edu.model.challenge.ChallengeUserRequest;
+import com.ssafy.edu.model.challenge.*;
 import com.ssafy.edu.model.user.User;
 import com.ssafy.edu.repository.ChallengeJpaRepository;
 import com.ssafy.edu.repository.ChallengeUsersJpaRepository;
@@ -14,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,7 +31,19 @@ public class ChallengeServiceImpl implements ChallengeService{
         ChallengeResponse result = new ChallengeResponse();
         List<Challenge> challengeList = challengeJpaRepository.findAll();
         if(!challengeList.isEmpty()){
-            result.data = challengeList;
+            List<ChallengeListForm> challengeListFormList = new ArrayList<>();
+            for(Challenge ch: challengeList){
+                ChallengeListForm tmp_form = ChallengeListForm.builder()
+                        .challengeId(ch.getId())
+                        .title(ch.getTitle())
+                        .Image(ch.getImage())
+                        .StartDate(ch.getStartDate())
+                        .EndDate(ch.getEndDate())
+                        .peopleCnt(ch.getPeopleCnt())
+                        .build();
+                challengeListFormList.add(tmp_form);
+            }
+            result.data = challengeListFormList;
             result.status = true;
         }else {
             result.status = false;
@@ -47,11 +57,24 @@ public class ChallengeServiceImpl implements ChallengeService{
         Optional<User> userOpt = userJpaRepository.findByEmail(challengeUserRequest.getEmail());
         List<ChallengeUser> challengeList = challengeUsersJpaRepository.findByUserAndDone(userOpt.get(), challengeUserRequest.getTodo());
         if(!challengeList.isEmpty()){
-            result.data = challengeList;
+            List<ChallengeListForm> challengeListFormList = new ArrayList<>();
+            for(ChallengeUser ch: challengeList){
+                Challenge tmp_challenge = ch.getChallenge();
+                ChallengeListForm tmp_form = ChallengeListForm.builder()
+                        .challengeId(tmp_challenge.getId())
+                        .title(tmp_challenge.getTitle())
+                        .Image(tmp_challenge.getImage())
+                        .StartDate(tmp_challenge.getStartDate())
+                        .EndDate(tmp_challenge.getEndDate())
+                        .peopleCnt(tmp_challenge.getPeopleCnt())
+                        .build();
+                challengeListFormList.add(tmp_form);
+            }
+            result.data = challengeListFormList;
             result.status = true;
-        }else {
-            result.status = false;
+            return new ResponseEntity<>(result, HttpStatus.OK);
         }
+        result.status = false;
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
@@ -61,15 +84,27 @@ public class ChallengeServiceImpl implements ChallengeService{
         Optional<User> userOpt = userJpaRepository.findByEmail(challengeUserRequest.getEmail());
         Optional<Challenge> challengeOpt = challengeJpaRepository.findById(challengeId);
         if(userOpt.isPresent()){
-            ChallengeUser.ChallengeUserBuilder challengeUser = ChallengeUser.builder()
+            ChallengeUser challengeUser = ChallengeUser.builder()
                     .user(userOpt.get())
                     .challenge(challengeOpt.get())
-                    .done(challengeUserRequest.getTodo());
+                    .done(challengeUserRequest.getTodo())
+                    .build();
+            ChallengeUser save = challengeUsersJpaRepository.save(challengeUser);
+
+            ChallengeListForm tmp_form = ChallengeListForm.builder()
+                    .challengeId(challengeOpt.get().getId())
+                    .title(challengeOpt.get().getTitle())
+                    .Image(challengeOpt.get().getImage())
+                    .StartDate(challengeOpt.get().getStartDate())
+                    .EndDate(challengeOpt.get().getEndDate())
+                    .peopleCnt(challengeOpt.get().getPeopleCnt())
+                    .build();
             if(challengeUserRequest.getTodo() == "done") {
-                Long tmp_people = challengeOpt.get().getPeopleCnt();
-                challengeOpt.get().setPeopleCnt(tmp_people + 1);
+                Long tmp_people = tmp_form.getPeopleCnt();
+                tmp_form.setPeopleCnt(tmp_people + 1);
             }
-            result.data = challengeOpt.get();
+
+            result.data = tmp_form;
             result.status = true;
         }else {
             result.status = false;
