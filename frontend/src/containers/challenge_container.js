3eeedfc4d85f1cc22bfc90fa49237effdd6dac8f
성진옥 <wlsdhr0831@naver.com>
2021-02-08 04:01:41 +0900

    [feat] make challenge containers

--- frontend/src/containers/challenge_container.js
+++ frontend/src/containers/challenge_container.js
@@ -1,22 +1,71 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import ChallengeMain from '../components/blockly/challenge/challenge_main/challenge_main';
 import * as ChallengeAction from '../modules/challenge';
 
 const ChallengeContainer = () => {
 
-  const allChallengeInfo = useSelector(state => state.challenge.allBlcoksInfo);
+  // store에 있는 state와 dispatch 가져오는 작업
+  const userInfo = useSelector(state => state.user.userInfo);
+  const allBlcoksInfo = useSelector(state => state.challenge.allBlcoksInfo);
+  const challengeList = useSelector(state => state.challenge.challengeList);
   const dispatch = useDispatch();
 
-  const getAllChallenge = async () => {
-    // 유저 메일 넣어야 할 것으로 예상
-    dispatch(ChallengeAction.getAllChallenge());
+  useEffect(() => {
+    getChallengeList();
+  }, []);
+
+  /*
+    api 요청 보내는 함수
+  */
+  // 전체 챌린지 목록 불러오기 ( 최신순 )
+  const getChallengeList = async () => {
+    try{
+      dispatch(ChallengeAction.getChallengeList());
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  // 특정 유저가 참가중인 챌린지 목록 불러오기
+  const getMyTodoChallengeList = async () => {
+    try{
+      dispatch(ChallengeAction.getMyChallengeList(
+        { email: userInfo.email, todo: 'todo'}
+      ));
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  // 특정 유저가 참가중인 챌린지 목록 불러오기
+  const getMyDoneChallengeList = async () => {
+    try{
+      dispatch(ChallengeAction.getMyChallengeList(
+        { email: userInfo.email, todo: 'Done'}
+      ));
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  // 현재 로그인한 유저가 챌린지 참여하기
+  const setTodoChallenge = async ( id ) => {
+    try{
+      dispatch(ChallengeAction.setTodoChallenge(
+        { email: userInfo.email, todo: 'todo', challengeId: id }
+      ));
+      getChallengeList();
+    } catch(e) {
+      console.log(e);
+    }
   }
 
   return (
     <>
       <ChallengeMain 
-        allChallengeInfo={allChallengeInfo}
+        challenges={challengeList}
+        setTodoChallenge={setTodoChallenge}
       />
     </>
   );
