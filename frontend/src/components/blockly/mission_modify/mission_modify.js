684d7198aa926d8ce380a58ff01aa976a10f6fba
성진옥 <wlsdhr0831@naver.com>
2021-02-09 01:43:37 +0900

    [feat] mission modify form css

--- frontend/src/components/blockly/mission_modify/mission_modify.js
+++ frontend/src/components/blockly/mission_modify/mission_modify.js
@@ -1,46 +1,52 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './mission_modify.module.css'
 
 const MissionModify = ({ title, content, onModifyMission, closeModal }) => {
   // props로 title content 받아서 value로 주면 끝!
+  const [ modifyInput, setModifyInput ] = useState({
+    title: title,
+    content: content,
+  });
 
-  const formRef = useRef();
-  const titleRef = useRef();
-  const contentRef = useRef();
+  const onChange = (e) => {
+    const { name, value } = e.target;
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    const titleContent = {
-      title: titleRef.current.value || '',
-      content: contentRef.current.value || '',
-    };
-    formRef.current.reset();
-    
-    // 여기 밑에 props로 받은 function에 titleContent 담아서 보내면 됨.
-    onModifyMission(titleContent)
+    setModifyInput({
+      ...modifyInput,
+      [name]: value,
+    })
   };
 
+  const onSubmit = () => {
+    onModifyMission(modifyInput);
+    closeModal();
+  };
 
   return (
     <>
-      <form ref={formRef}>
-        <h3>{title}</h3><br/>
+      <div className={styles.mission_modify}>
         <input 
           type="text" 
-          ref={titleRef} 
           name="title"
-          value={title}
-        /><br/>
-
-        <h3>{content}</h3><br/>
-          <textarea 
-            ref={contentRef}
-            name="content"
-            value={content}
-          /><br/>
-        <button onClick={onSubmit}>수정하기</button>
-        <button onClick={closeModal}>이전</button>
-      </form>
+          value={modifyInput.title}
+          placeholder="미션 제목"
+          onChange={onChange}
+          className={styles.titleInput}/>
+        <textarea 
+          name="content"
+          value={modifyInput.content}
+          placeholder="미션 내용"
+          onChange={onChange}
+          className={styles.contentInput}/><br/>
+        <button 
+          className={styles.btn_submit_modify}
+          onClick={onSubmit}>
+            수정하기</button>
+        <button
+          className={styles.btn_go_back} 
+          onClick={closeModal}>
+            이전</button>
+      </div>
     </>
   )
 }
