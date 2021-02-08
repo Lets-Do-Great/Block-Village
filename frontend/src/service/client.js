90fce6d15aea681b9e1f9e4e700e975ab94de187
성진옥 <wlsdhr0831@naver.com>
2021-02-08 17:22:00 +0900

    [feat] complete mission create API

--- frontend/src/service/client.js
+++ frontend/src/service/client.js
@@ -9,7 +9,7 @@ const client = axios.create();
 
 client.defaults.baseURL = 'http://i4b205.p.ssafy.io/api';
 
-client.defaults.headers.common['token'] = ``;
+client.defaults.headers.common['token'] = localStorage.getItem('token');
 client.defaults.headers.common['Content-Type'] = `application/json`;
 
 export default client;
