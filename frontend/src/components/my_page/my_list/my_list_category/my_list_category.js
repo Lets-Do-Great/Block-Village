b2d1d5bcf7f5f55df4db072a3db91acc34ae682e
성진옥 <wlsdhr0831@naver.com>
2021-02-10 04:23:36 +0900

    [feat] change category button css in my mission

--- frontend/src/components/my_page/my_list/my_list_category/my_list_category.js
+++ frontend/src/components/my_page/my_list/my_list_category/my_list_category.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import styles from './my_list_category.module.css';
 
 const MyListCategory = ({ onChangeCategory }) => {
-    return (<div>
-        <button 
-            onClick={onChangeCategory}
-            value="myMission">내가 만든 미션</button>
-        <button 
-            onClick={onChangeCategory}
-            value="makingAnswer">참여중인 미션</button>
-        <button 
-            onClick={onChangeCategory}
-            value="myAnswer">내가 만든 답안</button>
+    return (
+    <div className={styles.tab_menus}>
+        <ul className={styles.tabs}>
+            <li 
+                value="myMission"
+                onClick={onChangeCategory}
+                className={styles.current}>내가 만든 미션</li>
+            <li
+                value="makingAnswer"
+                onClick={onChangeCategory}>참여중인 미션</li>
+            <li 
+                value="myAnswer"
+                onClick={onChangeCategory}>내가 만든 답안</li>
+        </ul>
     </div>);
 }
 
