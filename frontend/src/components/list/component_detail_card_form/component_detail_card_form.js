d4c544825283357b78be8d88b3da457f342628d7
박승범 <parkbum11@naver.com>
2021-02-14 22:40:52 +0900

    [feat] answer detail css

--- frontend/src/components/list/component_detail_card_form/component_detail_card_form.js
+++ frontend/src/components/list/component_detail_card_form/component_detail_card_form.js
@@ -12,8 +12,10 @@ import '../../blockly/all_blocks/movement_blocks';
 import '../../blockly/all_blocks/drawing_blocks';
 import '../../blockly/all_blocks/function_blocks';
 
+import { IoMdArrowRoundBack } from 'react-icons/io';
 
-const ComponentDetailCardForm = ({ detail, setLike, setDisLike, 
+
+const ComponentDetailCardForm = ({ detail, setLike, setDislike, 
                             userInfo, closeDetail, onModify, onDelete }) => {
     const {email, nickname, title, created_at, updated_at, favorite,
         content, difficulty, likeCnt, readCnt } = detail;
@@ -27,72 +29,72 @@ const ComponentDetailCardForm = ({ detail, setLike, setDisLike,
     //====그 중 바꾸면 안되는 data 이거는 고정으로 둬야함
     const toolboxCategories = [];
     const theme = {
-        'blockStyles' : {
-          "start-blocks": {
-            "colourPrimary": "#C30D23",
-            "colourSecondary":"#AD7BE9",
-            "colourTertiary":"#141414"
-          },
-    
-          "judgement-blocks": {
-            "colourPrimary": "#FFA31D",
-             "colourSecondary":"#AD7BE9",
-            "colourTertiary":"#141414"
-          },
-    
-          "movement-blocks": {
-            "colourPrimary": "#8FC31F",
-            "colourSecondary":"#AD7BE9",
-            "colourTertiary":"#141414"
-          },
-    
-          "flow-blocks": {
-            "colourPrimary": "#55CFFF",
-            "colourSecondary":"#AD7BE9",
-            "colourTertiary":"#141414"
-          },
-          "calculation-blocks": {
-            "colourPrimary": "#1060FF",
-            "colourSecondary":"#AD7BE9",
-            "colourTertiary":"#141414"
-          },
-          "drawing-blocks": {
-            "colourPrimary": "#7D10C4",
-            "colourSecondary":"#AD7BE9",
-            "colourTertiary":"#141414"
-          },
-          "function-blocks": {
-            "colourPrimary": "#CC6666",
-            "colourSecondary":"#AD7BE9",
-            "colourTertiary":"#141414"
-          },
+      'blockStyles' : {
+        "start-blocks": {
+          "colourPrimary": "#C30D23",
+          "colourSecondary":"#AD7BE9",
+          "colourTertiary":"#141414"
         },
-        'componentStyles' : {
-          // 'workspaceBackgroundColour': '#1e1e1e',
-          'toolboxBackgroundColour': '#F7C469',
-          'toolboxForegroundColour': '#FFFFFF',
-          'flyoutBackgroundColour': '#FFDEA4',
-          'flyoutForegroundColour': '#1e1e1e',
-          'flyoutOpacity': '#1e1e1e',
-          'scrollbarColour': '#EFA420',
-          'scrollbarOpacity': 0.5,
-          // 'insertionMarkerColour': '#1e1e1e',
-          // 'insertionMarkerOpacity': '#1e1e1e',
+  
+        "judgement-blocks": {
+          "colourPrimary": "#FFA31D",
+            "colourSecondary":"#AD7BE9",
+          "colourTertiary":"#141414"
         },
-        'fontStyle': {
-          // "family": "Georgia, serif",
-          "weight": "bold",
-          "size": 12,
-        }
-      };
+  
+        "movement-blocks": {
+          "colourPrimary": "#8FC31F",
+          "colourSecondary":"#AD7BE9",
+          "colourTertiary":"#141414"
+        },
+  
+        "flow-blocks": {
+          "colourPrimary": "#55CFFF",
+          "colourSecondary":"#AD7BE9",
+          "colourTertiary":"#141414"
+        },
+        "calculation-blocks": {
+          "colourPrimary": "#1060FF",
+          "colourSecondary":"#AD7BE9",
+          "colourTertiary":"#141414"
+        },
+        "drawing-blocks": {
+          "colourPrimary": "#7D10C4",
+          "colourSecondary":"#AD7BE9",
+          "colourTertiary":"#141414"
+        },
+        "function-blocks": {
+          "colourPrimary": "#CC6666",
+          "colourSecondary":"#AD7BE9",
+          "colourTertiary":"#141414"
+        },
+      },
+      'componentStyles' : {
+        // 'workspaceBackgroundColour': '#1e1e1e',
+        'toolboxBackgroundColour': '#F7C469',
+        'toolboxForegroundColour': '#FFFFFF',
+        'flyoutBackgroundColour': '#FFDEA4',
+        'flyoutForegroundColour': '#1e1e1e',
+        'flyoutOpacity': '#1e1e1e',
+        'scrollbarColour': '#EFA420',
+        'scrollbarOpacity': 0.5,
+        // 'insertionMarkerColour': '#1e1e1e',
+        // 'insertionMarkerOpacity': '#1e1e1e',
+      },
+      'fontStyle': {
+        // "family": "Georgia, serif",
+        "weight": "bold",
+        "size": 12,
+      }
+    };
     //================================
 
     const changeLike = () => {
-        if(favorite){
-            setDisLike();
-        } else {
-            setLike();
-        }
+      if(favorite){
+        setDislike();
+      } else {
+        setLike();
+      }
     };
 
     const onSubmitDelete = () => {
@@ -105,87 +107,123 @@ const ComponentDetailCardForm = ({ detail, setLike, setDisLike,
     }
 
     return (
-        <>
-            <button onClick={closeDetail}>뒤로가기</button>
+      <div className={styles.body}>
+        <div className={styles.container}>
+          <div className={styles.pannel}>
 
-            <div className={styles.title}>{ title }</div>
-            <div 
-                className={styles.heart}
-                onClick={changeLike}>
-                { favorite
-                    ? <Icon.MdFavorite/>
-                    : <Icon.MdFavoriteBorder/> }
-            </div>
 
-            <div>
+
+            <div className={styles.header}>
+
+              <div className={styles.back_btn} onClick={closeDetail}>
+                <IoMdArrowRoundBack size="18" />
+                뒤로가기
+              </div>
+              <div className={styles.head_title}>{ title }</div>
+              { 
+                userInfo === email  
+                ? (
+                  <div className={styles.header_btn_group}>
+                    <div className={styles.header_btn_fix} onClick={onModify}>수정하기</div>
+                    <div className={styles.header_btn_dele} onClick={onSubmitDelete}>삭제하기</div>
+                  </div>
+                )
+                : (
+                  <div className={styles.header_btn_group}>
+                    <div className={styles.header_btn_fix}></div>
+                    <div className={styles.header_btn_dele}></div>
+                  </div>
+                )
+              }
+              <div className={styles.header_line}></div>
+
+              <div className={styles.icon}>
                 <Icon.MdAccountCircle/>{ nickname }
+              </div>
+              <div className={styles.icon}>
+                  <Icon.MdEdit/>{ created_at } / { updated_at }
+              </div>
+              <div className={styles.icon}>
+                  <Icon.MdAssistantPhoto/>{ difficulty }
+              </div>
+              <div className={styles.icon}>
+                  <Icon.MdFace/>{ readCnt }
+              </div>
+              <div className={styles.icon}>
+                  <Icon.MdFavorite/>{ likeCnt }
+              </div>
             </div>
-            <div>
-                <Icon.MdEdit/>{ created_at } / { updated_at }
+
+
+            <div className={styles.section}>
+              <div className={styles.playground}>
+                <AnswerPlayground 
+                  startPosition={startPosition}
+                  javascript_code={javascriptCode}
+                />
+              </div>
+
+              <div className={styles.workspace_cata}>
+                <div className={styles.workspace}>
+                  <ReactBlockly
+                    toolboxCategories={toolboxCategories}
+                    initialXml={xmlCode}
+                    wrapperDivClassName={styles.fill_height}
+                    workspaceConfiguration={{
+                        grid: {
+                            spacing: 20,
+                            length: 2,
+                            colour: '#FFDEA4',
+                            snap: true,
+                        },
+                        move:{
+                            scrollbars: true,
+                            drag: true,
+                            wheel: false,
+                        },
+                        zoom:{
+                            controls: true,
+                            wheel: true,
+                            startScale: 1.0,
+                            maxScale: 3,
+                            minScale: 0.3,
+                            scaleSpeed: 1.2,
+                            pinch: true
+                        },
+                        trashcan: true,
+                        renderer : "Zelos",
+                        theme: theme,
+                        readOnly: true,
+                    }}
+                  />
+                </div>
+              </div>
             </div>
-            <div className={styles.icon}>
-                <Icon.MdAssistantPhoto/>{ difficulty }
-            </div>
-            <div className={styles.icon}>
-                <Icon.MdFace/>{ readCnt }
-            </div>
-            <div className={styles.icon}>
-                <Icon.MdFavorite/>{ likeCnt }
-            </div>
+
+            
+
+            
 
             <div className={styles.content}>{ content }</div>
 
-            { userInfo === email && (<>
-                <button onClick={onModify}>수정하기</button>
-                <button onClick={onSubmitDelete}>삭제하기</button>
-            </>)}
+            
 
 
 
-            <div className={styles.playground}>
-                <AnswerPlayground 
-                    startPosition={startPosition}
-                    javascript_code={javascriptCode}
-                />
+            
+
+            <div 
+              className={styles.heart}
+              onClick={changeLike}>
+              { favorite
+                  ? <Icon.MdFavorite/>
+                  : <Icon.MdFavoriteBorder/> }
             </div>
 
-            <div className={styles.workspace_cata}>
-                <div className={styles.workspace}>
-                    <ReactBlockly
-                        toolboxCategories={toolboxCategories}
-                        initialXml={xmlCode}
-                        wrapperDivClassName={styles.fill_height}
-                        workspaceConfiguration={{
-                            grid: {
-                                spacing: 20,
-                                length: 2,
-                                colour: '#FFDEA4',
-                                snap: true,
-                            },
-                            move:{
-                                scrollbars: true,
-                                drag: true,
-                                wheel: false,
-                            },
-                            zoom:{
-                                controls: true,
-                                wheel: true,
-                                startScale: 1.0,
-                                maxScale: 3,
-                                minScale: 0.3,
-                                scaleSpeed: 1.2,
-                                pinch: true
-                            },
-                            trashcan: true,
-                            renderer : "Zelos",
-                            theme: theme,
-                            readOnly: true,
-                        }}
-                        // workspaceDidChange={workspaceDidChange}
-                    />
-                </div>
-            </div>
-        </>
+
+          </div>
+        </div>
+      </div>
     );
 }
 
