92ff804468a2488a15bebf70d5d2da91f56922af
성진옥 <wlsdhr0831@naver.com>
2021-02-16 02:23:42 +0900

    [fix] add search icon in mission list

--- frontend/src/components/list/search_form/search_form.js
+++ frontend/src/components/list/search_form/search_form.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import styles from './search_form.module.css';
+import { MdSearch } from "react-icons/md";
 
 const SearchForm = ({ type, onChangeSearch, onChangeSearchType, onClickEnter, search }) => {
     return (<>
 
     { type === 'mission' && 
-        <select name="sortType" className={styles.select_sort_type} onChange={onChangeSearchType}>
-            <option value="decrease">높은 순</option>
-            <option value="increase">낮은 순</option>
+        <select name="sortType" 
+            className={styles.select_sort_type} 
+            onChange={onChangeSearchType}>
+                <option value="decrease">높은 순</option>
+                <option value="increase">낮은 순</option>
         </select>
     }
 
     <div className={styles.search_form}>
-        <select name="keywordType" className={styles.select_search_form} onChange={onChangeSearchType}>
-            <option 
-                className={styles.option}
-                value="title">제목</option>
-            <option 
-                className={styles.option}
-                value="user">만든 이</option>
-        </select>
+        <MdSearch className={styles.icon_search}/>
         
         <input
             className={styles.input}
@@ -28,8 +24,18 @@ const SearchForm = ({ type, onChangeSearch, onChangeSearchType, onClickEnter, se
             value={search.keyword}
             onChange={onChangeSearch}
             onKeyDown={onClickEnter}
-            placeholder="검색어를 입력하세요."
-        />
+            placeholder="검색어를 입력하세요."/>
+
+        <select name="keywordType" 
+            className={styles.select_search_form} 
+            onChange={onChangeSearchType}>
+            <option 
+                className={styles.option}
+                value="title">제목</option>
+            <option 
+                className={styles.option}
+                value="user">만든 이</option>
+        </select>
     </div>
     </>);
 }
