814a57c1c0d7e4815a04d848086e51ceb3b56303
성진옥 <wlsdhr0831@naver.com>
2021-02-16 04:29:20 +0900

    [style] change css about mission and answer

--- frontend/src/containers/mission_container.js
+++ frontend/src/containers/mission_container.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import * as MissionAction from '../modules/mission';
 import ListForm from '../components/list/list_form/list_form';
-import SearchForm from '../components/list/search_form/search_form';
 import SearchType from '../components/list/search_type/search_type';
 import Nav from '../components/nav/nav';
-import * as MissionAction from '../modules/mission';
 
 const MissionContainer = () => {
     // 현재 검색 조건 데이터
